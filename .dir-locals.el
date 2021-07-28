5d5670906d8e61a99e6e49fed9a3344984528603
wangtianshu <wang@tianshu.me>
2021-07-28 19:51:36 +0800

    .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((eval . (git-auto-commit-mode 1))
+         (gac-debounce-interval . 600))))
