101e76025ac450b4a4a81c0f49025c97a73920cd
wangtianshu <wang@tianshu.me>
2021-08-19 21:47:55 +0800

    update scripts

---
+++ scripts/mmts_wdc.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import json
+import multiprocessing
+import subprocess
+from multiprocessing import Pool
+from string import Template
+from pathlib import Path
+
+from rich import print
+
+
+PROJECT_DIR = Path(__file__).parent.parent
+EXP_DIR = PROJECT_DIR / "logs" / "mmtsmatcher_wdcdatamodule"
+EXP_DIR.mkdir(parents=True, exist_ok=True)
+
+DEFAULT_ARGS = {
+    "model_name": "bert-base-chinese",
+    "feature_type": "grid",
+    "num_image_embeds": 1,
+    "cate": "all",
+    "training_size": "small",
+    "use_image": False,
+    "seed": 123,
+}
+EXPT_TMP = Template(
+    """{
+  "model": {
+    "class_path": "src.MMTSMatcher",
+    "init_args": {
+      "model_name": "${model_name}",
+      "feature_type": "${feature_type}",
+      "num_image_embeds": "${num_image_embeds}"
+    }
+  },
+  "data": {
+    "class_path": "src.WDCDataModule",
+    "init_args": {
+      "cate_level_name": "${cate_level_name}",
+      "cate_name": "${cate_name}",
+      "use_image": "${use_image}",
+      "prod_num": "${prod_num}"
+    }
+  },
+  "seed": "${seed}"
+}"""
+)
+EXPTS = []
+SEEDS = [123, 42]
+
+for training_size in ["small", "medium", "large", "xlarge"]:
+    for cate in ["all", "cameras", "computers", "shoes", "watches"]:
+        for seed in SEEDS:
+            kwargs_list = [
+                {"use_image": False},
+                {"use_image": True, "feature_type": "grid", "num_image_embeds": 1},
+                {"use_image": True, "feature_type": "grid", "num_image_embeds": 5},
+                {"use_image": True, "feature_type": "roi", "num_image_embeds": 5},
+            ]
+            use_image = False
+
+            for kwarg in kwargs_list:
+                kwarg["seed"] = seed
+                kwarg["cate"] = cate
+                kwarg["training_size"] = training_size
+                EXPTS.append(EXPT_TMP.substitute(DEFAULT_ARGS, **kwarg))
+
+
+def argument_parser():
+    parser = argparse.ArgumentParser(description="run experiments in parallel")
+    parser.add_argument(
+        "--fast-dev-run",
+        nargs="?",
+        type=int,
+        default=5,
+        help="whether to run command or not",
+    )
+    parser.add_argument(
+        "--no-run", action="store_true", help="whether to run command or not"
+    )
+    parser.add_argument("--gpus", nargs="+", default=["0"], help="availabled gpus")
+
+    return parser
+
+
+def run(exp_args, args):
+    worker_id = int(multiprocessing.current_process().name.rsplit("-", 1)[1]) - 1
+    gpu = args.gpus[worker_id]
+
+    exp_name = "_".join(map(str, exp_args["data"]["init_args"].values()))
+    exp_name += "_" + "_".join(map(str, exp_args["model"]["init_args"].values()))
+
+    outfile = EXP_DIR / (exp_name + "_out.log")
+    errfile = EXP_DIR / (exp_name + "_err.log")
+
+    if args.fast_dev_run:
+        cmd = f"""./run.py \\
+        --seed_everything {exp_args['seed']} \\
+        --trainer.gpus {gpu}, --trainer.fast_dev_run {args.fast_dev_run} \\
+        --model '{exp_args['model']}' \\
+        --data '{exp_args['data']}'"""
+    else:
+        cmd = f"""./run.py \\
+        --seed_everything {exp_args['seed']} \\
+        --trainer.gpus {gpu}, \\
+        --model '{exp_args['model']}' \\
+        --data '{exp_args['data']}' \\
+        >{outfile} 2>{errfile}
+        """
+
+    print(cmd)
+
+    if not args.no_run:
+        subprocess.call(cmd, shell=True)
+        (EXP_DIR / exp_name).touch()
+
+
+if __name__ == "__main__":
+    args = argument_parser().parse_args()
+    pool = Pool(processes=len(args.gpus))
+
+    for expt in EXPTS:
+        pool.apply_async(
+            run, kwds={"exp_args": json.loads(expt, strict=False), "args": args,},
+        )
+
+    pool.close()
+    pool.join()
