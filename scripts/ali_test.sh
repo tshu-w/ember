705f9f6037e5788dd928c4065dd824237dd4591d
wangtianshu <wang@tianshu.me>
2021-09-01 20:22:18 +0800

    ali_test.sh

--- scripts/ali_test.sh
+++ scripts/ali_test.sh
@@ -2,9 +2,9 @@
 
 export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890 no_proxy=localhost,127.0.0.0/8,*.local
 
-FILE="./results/mmtsmatcher_alidatamodule/*/config.yaml"
+FILE="./results/multimodalmatcher_alidatamodule/*/config.yaml"
 
 for f in $FILE; do
-    ./run.py --config $f --fit false --trainer.gpus 3, --trainer.default_root_dir tests
+    ./run.py --config $f --fit false --trainer.gpus 5, --trainer.default_root_dir tests --data.init_args.out_domain true
 done
 
