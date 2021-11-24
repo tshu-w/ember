3d585c924fdf36215a410d7e2e8204eb35ac083a
wangtianshu <wang@tianshu.me>
2021-11-24 22:17:42 +0800

    delete .dir-locals

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil . ((eval . (git-auto-commit-mode 1))
-         (gac-debounce-interval . 600))))
